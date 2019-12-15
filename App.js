f865a1433ae3d020f0e9bb2de814daabe8af6f76
Alex <fre3run@gmail.com>
2019-12-15 21:57:44 +0200

    fixed basic sms screen

--- App.js
+++ App.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TextInput, Button } from 'react-native';
 import firebase from 'react-native-firebase';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      number: ''
+    }
+  }
 
   componentDidMount() {
     firebase.auth().onAuthStateChanged((user) => {
@@ -11,7 +18,11 @@ class App extends Component {
         // continue code here
       }
     });
-    firebase.auth().signInWithPhoneNumber('+972544587647') // phone number format +972-5x-xxx-xx-xx
+  }
+
+  sendSMS = () => {
+    console.log(this.state.number);
+    firebase.auth().signInWithPhoneNumber(`+${this.state.number}`) // phone number format +972-5x-xxx-xx-xx
       .then((confirmResult) => {
         // confirmResult.confirm(verificationCode)
       })
@@ -20,10 +31,16 @@ class App extends Component {
       });
   }
 
+  setNumber = (number) => {
+    this.setState({ number })
+  }
+
   render() {
     return (
-      <View>
-        <Text>Hi</Text>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Number Here: with(972)</Text>
+        <TextInput keyboardType='number-pad' style={{ width: 200, borderWidth: 1 }} onChangeText={this.setNumber} />
+        <Button title='Press' onPress={this.sendSMS} />
       </View>
     );
   }
