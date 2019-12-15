65e9535a5bf62f51873c0fb54ed41f3059f4a321
Alex <fre3run@gmail.com>
2019-12-15 21:49:30 +0200

    change package name

--- android/app/src/main/java/com/awesomeproject/MainActivity.java
+++ android/app/src/main/java/com/awesomeproject/MainActivity.java
@@ -1,4 +1,4 @@
-package com.awesomeproject;
+package com.user200.awesomeproject;
 
 import com.facebook.react.ReactActivity;
 
--- android/app/src/main/java/com/awesomeproject/MainApplication.java
+++ android/app/src/main/java/com/awesomeproject/MainApplication.java
@@ -1,4 +1,4 @@
-package com.awesomeproject;
+package com.user200.awesomeproject;
 
 import android.app.Application;
 import android.content.Context;
